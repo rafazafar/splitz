ef2ab83fab70d89f18895015f446fa1e3ad8522f
Zafar <rafazafar@gmail.com>
2023-05-11 00:04:48 +0900

    ignore terms for middleware

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,8 @@
 import { authMiddleware } from '@clerk/nextjs'
 
-export default authMiddleware({})
+export default authMiddleware({
+  ignoredRoutes: ['/terms'],
+})
 
 export const config = {
   matcher: ['/((?!.*\\..*|_next).*)', '/'],
